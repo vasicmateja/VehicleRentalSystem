5f25bb41d56717bef74c9c6cd1119bb16cb5c375
Mateja <106273674+vasicmateja@users.noreply.github.com>
2023-10-15 17:36:15 +0200

    ZullService

---
+++ ZuulService/src/main/java/com/SK_Prj2_Rakic_Vasic/ZuulService/ZuulServiceApplication.java
@@ -0,0 +1,34 @@
+package com.SK_Prj2_Rakic_Vasic.ZuulService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulServiceApplication {
+
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServiceApplication.class, args);
+	}
+
+}
---
+++ ZuulService/src/test/java/com/SK_Prj2_Rakic_Vasic/ZuulService/ZuulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SK_Prj2_Rakic_Vasic.ZuulService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
